2f3bb82360902abac2e79db18aca9dd76cae9c82
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-26 09:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const dateAndTime = document.querySelector("#timer");
+
+function updateTime() {
+	const current = new Date();
+	const options = {
+		month: "2-digit",
+		day: "2-digit",
+		year: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		hour12: true
+	};
+	const formateDateTime = current.toLocaleString(undefined, options);
+	dateAndTime.textContent = formateDateTime;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
