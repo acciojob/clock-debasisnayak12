07d54efbd3014b2d0d1daa82257f3160bacb8518
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-05 19:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 const dateAndTime = document.querySelector("#timer");
 
-function updateTime() {
-	const current = new Date();
-	const options = {
-		month: "2-digit",
-		day: "2-digit",
-		year: "2-digit",
-		hour: "2-digit",
-		minute: "2-digit",
-		second: "2-digit",
-		hour12: true
-	};
-	const formateDateTime = current.toLocaleString(undefined, options);
-	const [date,timePart] = formateDateTime.split(', ');
-	const [time, ampm] = timePart.split(' ');
-	const capitalAmPm = ampm.toUpperCase();
-
-	const formate = `${date}, ${time} ${capitalAmPm}`;
-	dateAndTime.textContent = formate;
+setInterval(()=>{
+	let instance = new Date();
+	const date = `${instance.getMonth() + 1}/${instance.getDate()}/${instance.getFullYear()}`;
+	let hrs = instance.getHours();
+	const ampm = hrs >= 12 ? "PM" : "AM";
 	
-}
+	hrs > 12 && (hrs -= 12);
 
-updateTime();
-
-setInterval(updateTime, 1000);
+	const time = `${hrs}:${instance.getMinutes()}:${instance.getSeconds()} ${ampm}`;
+	dateAndTime.innerText = `${date}, ${time}`;
+	
+},1000);
